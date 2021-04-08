e890be245683aacf4e0d67f85f2bce87df840907
Ryan Mc Donald <S00199690@mail.itsligo.ie>
2021-04-08 01:03:41 +0100

    display tv show info

--- TvShowProject/MainWindow.xaml.cs
+++ TvShowProject/MainWindow.xaml.cs
@@ -20,9 +20,52 @@ namespace TvShowProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Show> allShows = new List<Show>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void winTvShows_Loaded(object sender, RoutedEventArgs e)
+        {
+            Show s1 = new Show() { Title = "Breaking Bad", Seasons = 5, Episodes = 62, YearStart = 2008, YearEnd = 2013, Description = "Breaking bad sees Chemistry teacher Walter White and drop out student Jesse Pinkman team up making meth. Not everything goes to plan." };
+            Show s2 = new Show() { Title = "Rick and Morty", Seasons = 4, Episodes = 41, YearStart = 2013, YearEnd = 2021, Description = "Animated adventures of mad scientist Rick Sanchez and schoolboy Morty Smith" };
+            Show s3 = new Show() { Title = "The Office", Seasons = 9, Episodes = 201, YearStart = 2005, YearEnd = 2013, Description = "Mockumentary of a normal day to day in an paper company" };
+            Show s4 = new Show() { Title = "Paradise PD", Seasons = 3, Episodes = 18, YearStart = 2018, YearEnd = 2021, Description = "Animated ventures of local police team" };
+            Show s5 = new Show() { Title = "Aunty Donnas Big Ol House Of Fun", Seasons = 1, Episodes = 6, YearStart = 2020, YearEnd = 2021, Description = "Morning Brown" };
+            
+            //add to collection
+            allShows.Add(s1);
+            allShows.Add(s2);
+            allShows.Add(s3);
+            allShows.Add(s4);
+            allShows.Add(s5);
+
+            //sort bands
+            allShows.Sort();
+
+            lbxShows.ItemsSource = allShows;
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void lbxShows_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Show selectedShow = lbxShows.SelectedItem as Show;
+
+            if(selectedShow != null)
+            {
+                tbkTitle.Text = selectedShow.Title;
+                tbkSeasons.Text = selectedShow.Seasons.ToString();
+                tbkEpisodes.Text = selectedShow.Episodes.ToString();
+                tbkYearStarted.Text = selectedShow.YearStart.ToString();
+                tbkYearEnded.Text = selectedShow.YearEnd.ToString();
+                tbkDescription.Text = selectedShow.Description;
+                
+            }
         }
     }
 }
--- TvShowProject/Show.cs
+++ TvShowProject/Show.cs
@@ -6,8 +6,49 @@ using System.Threading.Tasks;
 
 namespace TvShowProject
 {
-    class Show
+    public class Show : IComparable
     {
-        
+        //properties
+        public string Title { get; set; }
+        public int Seasons { get; set; }
+
+        public int Episodes { get; set; }
+
+        public int YearStart { get; set; }
+
+        public int YearEnd { get; set; }
+
+        public string Description { get; set; }
+
+        //constructors
+        public Show(string title, int seasons, int episodes, int yearStart, int yearEnd, string description)
+        {
+            Title = title;
+            Seasons = seasons;
+            Episodes = episodes;
+            YearStart = yearStart;
+            YearEnd = yearEnd;
+            Description = description;
+        }
+
+        public Show()
+        {
+
+        }
+
+        //methods
+
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Show otherShow = obj as Show;
+            return this.Title.CompareTo(otherShow.Title);
+        }
     }
+
+   
 }
